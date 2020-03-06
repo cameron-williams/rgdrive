360ec4cac5e440e59639bb6ec8c2f592e4f486d5
Cam Williams <cam@camwilliams.ca>
2020-03-05 22:14:43 -0800

    add basic update functionality for tracked files

--- src/bin/rgdrived.rs
+++ src/bin/rgdrived.rs
@@ -11,18 +11,15 @@ use std::io::{BufReader, BufWriter};
 use std::io::prelude::*;
 use std::process;
 
-use std::collections::HashSet;
+use std::collections::{HashSet, HashMap};
 
 use std::sync::{Arc, Mutex};
 use std::thread;
 
 use std::time::Duration;
 
-use inotify::{Inotify, WatchMask, EventMask};
+use inotify::{Inotify, WatchMask, EventMask, WatchDescriptor};
 use google_api::Drive;
-
-use serde_json::Value;
-
 
 
 const SOCKET_PATH: &str = "/tmp/rgdrive.sock";
@@ -42,7 +39,7 @@ fn get_stream_text(s: &mut UnixStream) -> String {
 }
 
 
-fn handle_stream(stream: &mut UnixStream, drive: Arc<Mutex<Drive>>, notify: Arc<Mutex<Inotify>>) {
+fn handle_stream(stream: &mut UnixStream, drive: Arc<Mutex<Drive>>, notify: Arc<Mutex<Inotify>>, tracked_files: Arc<Mutex<TrackedFiles>>) {
     let text = get_stream_text(stream);
     if text.len() == 0 {
         return
@@ -63,20 +60,21 @@ fn handle_stream(stream: &mut UnixStream, drive: Arc<Mutex<Drive>>, notify: Arc<
         "push" => {
             debug!("Push command received: {:?}", cmd);
             // Upload file to drive.
-            match drive.upload_file(
+            let url = match drive.upload_file(
                 PathBuf::from(cmd[1])
             ) {
-                Ok(_) => info!("uploaded {} successfully", cmd[1]),
+                Ok(url) => {
+                    info!("uploaded {} successfully", cmd[1]);
+                    url
+                },
                 Err(e) => {
                     error!("failed to upload {}: {:?}", cmd[1], e);
                     return
                 },
-            }
-            // After a successful upload, add path to Inotify watch list for modify/delete masks.
-            match notify.lock().unwrap().add_watch(cmd[1], WatchMask::MODIFY | WatchMask::DELETE) {
-                Ok(_) => debug!("{} added to Inotify watchlist successfully", cmd[1]),
-                Err(e) => error!("failed to add {} to the Inotify watchlist: {:?}", cmd[1], e),
             };
+            // After a successful upload, add path to TrackedFiles for modify/delete masks.
+            tracked_files.lock().unwrap().add_path(notify, cmd[1], url);
+        
         },
 
         "pull" => {
@@ -97,32 +95,84 @@ fn handle_stream(stream: &mut UnixStream, drive: Arc<Mutex<Drive>>, notify: Arc<
     }
 }
 
-
-
-struct InotifyTracker {
-    notify: Arc<Mutex<Inotify>>,
-    tracked_files: HashSet<String>,
+/// Write paths that need to be tracked to config file from TrackedFiles.map (HashMap<wd, String>)
+fn write_saved_inotify_events(e: &HashMap<WatchDescriptor, String>) {
+    // Ensure config path exists. If it doesn't create it.
+    let path = config_dir();
+    if !path.exists() {
+        match create_dir_all(path.parent().unwrap()) {
+            Ok(_) => {
+                if let Err(e) = File::create(&path) {
+                    panic!(format!("failed to create new config file: {:#?}", e));
+                }
+            }
+            Err(e) => panic!(format!("failed to create config dir: {:#?}", e)),
+        }
+    }
+    let mut vals = Vec::new();
+    e.values().for_each(|i| {vals.push(i)});
+    
+    match OpenOptions::new()
+        .read(true)
+        .write(true)
+        .truncate(true)
+        .open(path)
+    {
+        Ok(f) => {
+            let writer = BufWriter::new(f);
+            if let Err(e) = serde_json::to_writer_pretty(writer, &vals) {
+                panic!(format!(
+                    "error writing/serializing config to file: {:#?}",
+                    e
+                ));
+            } 
+        }
+        Err(e) => panic!(format!("error opening config file in write mode: {:#?}", e)),
+    }
 }
 
-impl InotifyTracker {
-    fn new(n: Arc<Mutex<Inotify>>) -> Self {
-        // On init, load any tracked files from config.
-        let mut t = Self {
-            notify: n,
-            tracked_files: HashSet::new()
-        };
-        t.tracked_files = InotifyTracker::read_from_config().unwrap();
-        for p in &t.tracked_files {
-            match t.notify.lock().unwrap().add_watch(p, WatchMask::MODIFY | WatchMask::DELETE) {
-                Ok(_) => debug!("{} added to Inotify watchlist successfully", p),
-                Err(e) => error!("failed to add {} to the Inotify watchlist: {:?}", p, e),
-            };
-            info!("tracked path: {}", p);
-        }
-        t
-    }
 
-    fn read_from_config() -> Result<HashSet<String>, String> {
+fn inotify_listen(notify: Arc<Mutex<Inotify>>, drive: Arc<Mutex<Drive>>, tracked_files: Arc<Mutex<TrackedFiles>>) {
+    let mut buffer = [0; 1024];
+    loop {
+        let events = notify.lock().unwrap().read_events(&mut buffer).expect("Failed to read inotify events");
+
+        for event in events {
+            match event.mask {
+                EventMask::MODIFY => {
+                    let entry = tracked_files.lock().unwrap();
+                    let path: Vec<&str> = match entry.map.get(&event.wd) {
+                        Some(p) => p.split(",").collect(),
+                        None => {
+                            error!("no matching entry in saved wds for {:?}", event.wd);
+                            continue
+                        },
+                    };
+                    debug!("modify event: {:#?}\nEntry: {:#?}", event.wd, path);
+                    match drive.lock().unwrap().update_file(PathBuf::from(path[0]), path[1]) {
+                        Ok(_) => info!("Successfully updated file: {:?}", path),
+                        Err(e) => error!("Error updating file {:?}: {:?}", path, e),
+                    }
+                    
+                },
+                EventMask::DELETE => {},
+                _ => {}
+            }
+        }
+        thread::sleep(Duration::from_millis(500));
+    }
+}
+
+
+
+struct TrackedFiles {
+    // Holds {WD: String("path,drive_url")
+    map: HashMap<WatchDescriptor, String>
+}
+
+impl TrackedFiles {
+    // Init tracked files, loading any from saved config.
+    fn from_config(notify: Arc<Mutex<Inotify>>) -> Self {
         // Ensure config path exists. If it doesn't create it and return a blank value
         let path = config_dir();
         if !path.exists() {
@@ -134,10 +184,9 @@ impl InotifyTracker {
                 }
                 Err(e) => panic!(format!("failed to create config dir: {:#?}", e)),
             }
-            return Ok(HashSet::new());
         }
         // Open file as readonly, and read vec of pathnames from file.
-        match OpenOptions::new()
+        let paths: HashSet<String> = match OpenOptions::new()
             .read(true)
             .write(false)
             .open(config_dir())
@@ -145,70 +194,50 @@ impl InotifyTracker {
             Ok(f) => {
                 let reader = BufReader::new(f);
                 match serde_json::from_reader(reader) {
-                    Ok(d) => Ok(d),
-                    Err(_) => Ok(HashSet::new()),
+                    Ok(d) => d,
+                    Err(_) => HashSet::new(),
                 }
             }
             Err(e) => panic!(format!("error reading from config file: {:#?}", e)),
+        };
+
+        let mut map: HashMap<WatchDescriptor, String> = HashMap::new();
+
+        for p in &paths {
+            let vals: Vec<&str> = p.split(",").collect();
+            match notify.lock().unwrap().add_watch(vals[0], WatchMask::MODIFY | WatchMask::DELETE) {
+                Ok(wd) => {
+                    debug!("{} added to Inotify watchlist successfully", p);
+                    map.insert(wd, p.to_string())
+                },
+                Err(e) => {
+                    error!("failed to add {} to the Inotify watchlist: {:?}", p, e);
+                    continue
+                },
+            };
         }
+        info!("{:#?}", map);
+
+        Self {map}
     }
 
-    fn write_path_to_config(&self) -> Result<(), String> {
-        // Ensure config path exists. If it doesn't create it.
-        let path = config_dir();
-        if !path.exists() {
-            match create_dir_all(path.parent().unwrap()) {
-                Ok(_) => {
-                    if let Err(e) = File::create(&path) {
-                        panic!(format!("failed to create new config file: {:#?}", e));
-                    }
-                }
-                Err(e) => panic!(format!("failed to create config dir: {:#?}", e)),
-            }
+    /// Adds given path to Inotify watch, as well as tracked files map.
+    fn add_path<P: Into<String>, U: Into<String>>(&mut self, notify: Arc<Mutex<Inotify>>, path: P, url: U) {
+        let path = path.into();
+        let url = url.into();
+        match notify.lock().unwrap().add_watch(path.clone(), WatchMask::MODIFY | WatchMask::DELETE) {
+            Ok(wd) => {
+                debug!("{} added to Inotify watchlist successfully", path);
+                self.map.insert(wd, format!("{},{}", path, url));
+            },
+            Err(e) => {
+                error!("failed to add {} to the Inotify watchlist: {:?}", path, e);
+                return
+            },
         }
-        // Write current state of self GoogleOAuthToken to file as json.
-        match OpenOptions::new()
-            .read(true)
-            .write(true)
-            .truncate(true)
-            .open(path)
-        {
-            Ok(f) => {
-                let writer = BufWriter::new(f);
-                if let Err(e) = serde_json::to_writer_pretty(writer, &self.tracked_files) {
-                    panic!(format!(
-                        "error writing/serializing config to file: {:#?}",
-                        e
-                    ));
-                } else {
-                    Ok(())
-                }
-            }
-            Err(e) => Err(format!("error opening config file in write mode: {:#?}", e)),
-        }
-    }
-
-    fn listen(&mut self) {
-        let mut buffer = [0; 1024];
-        loop {
-            let events = self.notify.lock().unwrap().read_events(&mut buffer).expect("Failed to read inotify events");
-            debug!("Checking for events...");
-
-            for event in events {
-                match event.mask {
-                    EventMask::MODIFY => {
-                        info!("modify event for {:?}", event.name);
-                        info!("modify event: {:#?}", event);
-                    },
-                    EventMask::DELETE => {},
-                    _ => {}
-                }
-            }
-            thread::sleep(Duration::from_millis(500));
-        }
+        write_saved_inotify_events(&self.map)
     }
 }
-
 
 
 fn main() {
@@ -252,23 +281,23 @@ fn main() {
         }
     };
 
+    // Holds all tracked paths.
+    let tracked_files = Arc::new(Mutex::new(TrackedFiles::from_config(Arc::clone(&inotify))));
+
     // Spawn a new thread which listens for and handles Inotify events.
     let inotify_clone = Arc::clone(&inotify);
     let drive_clone = Arc::clone(&drive);
-    thread::spawn(move || {
-        // inotify_listener(inotify_clone, drive_clone)
-        let mut t = InotifyTracker::new(inotify_clone);
-        t.listen();
-        // t.tracked_files.remove(&String::from("/home/cam/testfile2.txt"));
-        // t.write_path_to_config().unwrap();
+    let tracked_files_c = Arc::clone(&tracked_files);
 
+    thread::spawn(move || {
+        inotify_listen(inotify_clone, drive_clone, tracked_files_c);
     });
 
     // Listen for and handle incoming streams on the socket.
     for stream in listener.incoming() {
         match stream {
             Ok(mut s) => {
-                handle_stream(&mut s, Arc::clone(&drive), Arc::clone(&inotify));
+                handle_stream(&mut s, Arc::clone(&drive), Arc::clone(&inotify), Arc::clone(&tracked_files));
             },
             Err(e) => {
                 error!("stream err: {:?}", e);
