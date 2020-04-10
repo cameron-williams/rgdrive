e357edec306a7ccaa6e5c07a323a9c9aff5f90ac
Cam Williams <cam@camwilliams.ca>
2020-04-10 13:10:17 -0700

    auto format, fix save on tracker::remove_path()

--- src/lib.rs
+++ src/lib.rs
@@ -7,14 +7,13 @@ use std::fs::{File, OpenOptions};
 use std::io::prelude::*;
 use std::io::Error;
 
-use std::os::unix::net::UnixStream;
 use std::net::Shutdown;
+use std::os::unix::net::UnixStream;
 
 use std::time::Duration;
 
-use serde::{Deserialize, Serialize};
 use inotify::{Inotify, WatchDescriptor, WatchMask};
-
+use serde::{Deserialize, Serialize};
 
 pub const SOCKET_PATH: &str = "/tmp/rgdrive.sock";
 pub const CONFIG_PATH: &str = "/.config/cameron-williams/tracked_files";
@@ -25,7 +24,6 @@ pub fn config_dir() -> PathBuf {
     PathBuf::from(dir)
 }
 
-
 #[derive(Deserialize, Serialize, Debug)]
 pub enum DResult {
     Ok(String),
@@ -33,14 +31,11 @@ pub enum DResult {
 }
 
 impl DResult {
-    
     // Send result on stream.
     pub fn send(&self, mut s: &UnixStream) -> Result<(), Error> {
         // Set write timeout just in case the client isn't listening/ready for a response for some reason.
         s.set_write_timeout(Some(Duration::from_secs(15)))?;
-        s.write_all(
-            &bincode::serialize(&self).unwrap()
-        )?;
+        s.write_all(&bincode::serialize(&self).unwrap())?;
         Ok(())
     }
 
@@ -51,34 +46,28 @@ impl DResult {
     pub fn ok<M: Into<String>>(m: M) -> DResult {
         DResult::Ok(m.into())
     }
-
 }
 
 #[derive(Deserialize, Serialize, Debug)]
 pub enum DCommand {
     // Args are as followed: drive_url, path_to_download_to, overwrite
     Pull(String, PathBuf, bool),
-    
     // path_to_file_to_push
     Push(PathBuf),
-    
     // path_to_local_file, drive_url
     FSync(PathBuf, String),
-    
     // path_to_local_file
     FUnSync(PathBuf),
 
     None,
     Message(String),
     Ok,
-    Quit
+    Quit,
 }
 
-
 impl DCommand {
-
     // Initialize a DCommand from a &UnixStream. A reference since the stream will be used later to possibly send a response.
-    pub fn from_stream(mut s: &UnixStream) -> DCommand {        
+    pub fn from_stream(mut s: &UnixStream) -> DCommand {
         let mut buf: Vec<u8> = Vec::new();
         s.read_to_end(&mut buf).unwrap();
         if buf.len() > 0 {
@@ -89,19 +78,13 @@ impl DCommand {
     }
 }
 
-
-
 pub struct DSocket {
     path: PathBuf,
 }
 
-
 impl DSocket {
-
     pub fn new<P: Into<PathBuf>>(p: P) -> DSocket {
-        DSocket {
-            path: p.into()
-        }
+        DSocket { path: p.into() }
     }
 
     pub fn is_active(&self) -> bool {
@@ -114,14 +97,11 @@ impl DSocket {
 
     // Send given command to the daemon. Expects and will wait timeout duration for a response.
     pub fn send_command(&self, cmd: DCommand) -> Result<DResult, Error> {
-
         // Connect to stream.
         let mut stream = UnixStream::connect(&self.path)?;
 
         // Write command to stream.
-        stream.write_all(
-            &bincode::serialize(&cmd).unwrap()
-        )?;
+        stream.write_all(&bincode::serialize(&cmd).unwrap())?;
 
         // Shutdown write half of stream and set read timeout for response.
         stream.shutdown(Shutdown::Write)?;
@@ -130,21 +110,16 @@ impl DSocket {
         let mut buf: Vec<u8> = Vec::new();
         stream.read_to_end(&mut buf)?;
         let result: DResult = bincode::deserialize(&buf).unwrap();
-        
         Ok(result)
     }
 
     // Send given command to the daemon. Does not expect a response.
     pub fn send_command_no_response(&self, cmd: DCommand) -> Result<(), Error> {
         let mut stream = UnixStream::connect(&self.path)?;
-        stream.write_all(
-            &bincode::serialize(&cmd).unwrap()
-        )?;
+        stream.write_all(&bincode::serialize(&cmd).unwrap())?;
         Ok(())
     }
 }
-
-
 
 pub struct Tracker {
     pub inotify: Inotify,
@@ -152,9 +127,7 @@ pub struct Tracker {
     tracked_files_path: PathBuf,
 }
 
-
 impl Tracker {
-
     // Initialize Tracker.
     pub fn init() -> Tracker {
         let mut tracker = Tracker {
@@ -165,44 +138,43 @@ impl Tracker {
 
         // If we have an existing list of tracked files, open it and attempt to read it's contents.
         if tracker.tracked_files_path.exists() {
-
             // On a failed file read, just return tracker with empty tracked_files vec.
             let mut f = match File::open(&tracker.tracked_files_path) {
                 Ok(f) => f,
                 Err(e) => {
                     log::error!("Error opening tracked files config file: {:?}", e);
-                    return tracker
+                    return tracker;
                 }
             };
 
             // Read existing files to buf, and overwrite empty vec with any existing files.
             let mut buf: Vec<u8> = Vec::new();
             f.read_to_end(&mut buf).unwrap();
-            
             // Deserialize file to Vec<Trackedfile>
             let tracked_files: Vec<TrackedFile> = match bincode::deserialize(&buf) {
                 Ok(v) => v,
                 Err(e) => {
                     log::warn!("Error deserializing from file: {:?}.. Continuing anyways with a blank tracker.", e);
-                    return tracker
+                    return tracker;
                 }
             };
-            
             // Iterate any trackedfiles that were deseralized from file. Add watches for MODIFY, DELETE_SELF, and MOVE_SELF.
             // Update the TrackedFile resource to include the WatchDescriptor and add it back to the tracker tracked files list.
             for tf in tracked_files {
-                let wd = match tracker.inotify.add_watch(&tf.path, WatchMask::MODIFY | WatchMask::DELETE_SELF | WatchMask::MOVE_SELF) {
+                let wd = match tracker.inotify.add_watch(
+                    &tf.path,
+                    WatchMask::MODIFY | WatchMask::DELETE_SELF | WatchMask::MOVE_SELF,
+                ) {
                     Ok(wd) => wd,
                     Err(e) => {
                         log::error!("Failed to add {:?} to Inotify watch: {:?}", tf, e);
-                        continue
+                        continue;
                     }
                 };
                 log::info!("adding {:?} to watch", tf);
-                tracker.tracked_files.push(TrackedFile {
-                    wd: Some(wd),
-                    ..tf
-                });
+                tracker
+                    .tracked_files
+                    .push(TrackedFile { wd: Some(wd), ..tf });
             }
         }
         tracker
@@ -212,13 +184,11 @@ impl Tracker {
     fn save(&self) -> Result<(), Error> {
         // Open tracked files path. Create new so that it erases any existing paths, since they could have been changed or removed since the last time we accessed the file.
         let mut f = OpenOptions::new()
-                                .write(true)
-                                .truncate(true)
-                                .open(&self.tracked_files_path)?;    
+            .write(true)
+            .truncate(true)
+            .open(&self.tracked_files_path)?;
         // Serialize the tracked files vec and write it to the file.
-        f.write_all(
-            &bincode::serialize(&self.tracked_files).unwrap()
-        )?;
+        f.write_all(&bincode::serialize(&self.tracked_files).unwrap())?;
         Ok(())
     }
 
@@ -229,29 +199,35 @@ impl Tracker {
         // Check if path is already added to the watchlist. Skip path if it is.
         for tf in &self.tracked_files {
             if *tf.path == path {
-                return Ok(())
+                return Ok(());
             }
         }
 
         // Add path to inotify watchlist for specific WatchMasks.
-        let wd = match self.inotify.add_watch(&path, WatchMask::MODIFY | WatchMask::DELETE_SELF | WatchMask::MOVE_SELF) {
+        let wd = match self.inotify.add_watch(
+            &path,
+            WatchMask::MODIFY | WatchMask::DELETE_SELF | WatchMask::MOVE_SELF,
+        ) {
             Ok(wd) => {
                 log::debug!("added {:?} to the watchlist", wd);
                 wd
-            },
+            }
             Err(e) => {
-                log::error!("Failed to add {:?}{:?} to the inotify watchlist: {:?}", path, url, e);
+                log::error!(
+                    "Failed to add {:?}{:?} to the inotify watchlist: {:?}",
+                    path,
+                    url,
+                    e
+                );
                 return Err(e);
             }
         };
         // Add a trackedfile entry with the newly created WatchDescriptor.
-        self.tracked_files.push(
-            TrackedFile {
-                drive_url: url,
-                path: path,
-                wd: Some(wd),
-            }
-        );
+        self.tracked_files.push(TrackedFile {
+            drive_url: url,
+            path: path,
+            wd: Some(wd),
+        });
         // Save and write to file so new config will persist through sessions.
         self.save()?;
         Ok(())
@@ -259,7 +235,6 @@ impl Tracker {
 
     pub fn remove_path<P: Into<PathBuf>>(&mut self, p: P) -> Result<(), Error> {
         let path = p.into();
-        
         // Temp vec to hold drained TrackedFiles.
         let mut _tf: Vec<TrackedFile> = Vec::new();
         // Iterate all tracked files, if their patch matches remove them from the Inotify watchlist.
@@ -273,10 +248,10 @@ impl Tracker {
             }
         }
         self.tracked_files = _tf;
+        self.save()?;
         Ok(())
     }
 }
-
 
 #[derive(Deserialize, Serialize, Debug)]
 pub struct TrackedFile {
@@ -294,26 +269,22 @@ impl TrackedFile {
             Ok(f) => f,
             Err(e) => {
                 log::error!("Error opening tracked files config file: {:?}", e);
-                return Vec::new()
+                return Vec::new();
             }
         };
 
         // Read existing files to buf, and overwrite empty vec with any existing files.
         let mut buf: Vec<u8> = Vec::new();
         f.read_to_end(&mut buf).unwrap();
-        
         // Deserialize file to Vec<Trackedfile>
         match bincode::deserialize(&buf) {
-            Ok(v) => {
-                return v
-            },
+            Ok(v) => return v,
             Err(e) => {
                 log::warn!("Error deserializing from file: {:?}.. Continuing anyways with a blank tracker.", e);
-                return Vec::new()
+                return Vec::new();
             }
         };
     }
 }
-
 
 fn main() {}
